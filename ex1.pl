a3ab9f3e095b1275503b84498f915ee6a2426cb1
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-05-22 11:51:30 +0200

    ex 1

---
+++ ex1.pl
@@ -0,0 +1,7 @@
+count_occur(X, [], N) :- format("Element ~d occurrs ~d times. ~n", [X,N]).
+count_occur(X, [X|T], N) :- 
+	N2 is N + 1,
+    count_occur(X, T, N2).    
+count_occur(X, [Y|T], N) :- 
+    X \= Y,          
+    count_occur(X, T, N).
