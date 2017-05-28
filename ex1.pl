bdd9b36924a2ac22b7454199d07b709c00553f32
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-05-28 16:46:46 +0200

    Add comments

--- ex1.pl
+++ ex1.pl
@@ -1,9 +1,11 @@
-counter(X, L) :- count_occur(X, L, 0).
+counter(X, L) :-            % Calling predicate
+	count_occur(X, L, 0).   % Predicate counting occurrences
 
-count_occur(X, [], N) :- format("Element ~d occurrs ~d times. ~n", [X,N]).
-count_occur(X, [X|T], N) :- 
-	N2 is N + 1,
-    count_occur(X, T, N2).    
-count_occur(X, [Y|T], N) :- 
+count_occur(X, [], N) :-    % Base case, If list is empty then print message
+	format("Element ~d occurrs ~d times. ~n", [X,N]). 
+count_occur(X, [X|T], N) :- % If current item is the item that we are looking for
+	N2 is N + 1,            % Increment counter
+    count_occur(X, T, N2).  % Call recursively only using tail of the list and incremented counter
+count_occur(X, [Y|T], N) :- % If current item is not the item that we are looking for
     X \= Y,          
-    count_occur(X, T, N).
+    count_occur(X, T, N).   % Call recursively only using tail of the list and same counter
--- ex2.pl
+++ ex2.pl
@@ -1,8 +1,11 @@
-finder(X, L) :- get_index(X, L, 0).
+finder(X, L) :-             % Calling predicate
+	get_index(X, L, 0).     % Predicate getting index of the item if exist
 
-get_index(X, [], N) :- format("Element ~d not found. ~n", [X,N]).
-get_index(X, [X|T], N) :- format("Element ~d found at index ~d. ~n", [X,N]).  
-get_index(X, [Y|T], N) :- 
+get_index(X, [], N) :-      % First base case, If list if empty and element has not found then print message 
+	format("Element ~d not found. ~n", [X,N]).
+get_index(X, [X|T], N) :-   % Second base case, If list if empty and element has found then print message
+	format("Element ~d found at index ~d. ~n", [X,N]).  
+get_index(X, [Y|T], N) :-   % If current item is not the item that we are looking for
     X \= Y,          
-    N2 is N + 1,
-    get_index(X, T, N2). 
+    N2 is N + 1,            % Update index
+    get_index(X, T, N2).    % Call recursively to try to find the element
